04fdd56062fc03e8311a2bb8cc2cea0874e41772
NutanHiwale <hiwalenutan99@gmail.com>
2022-09-20 14:46:45 +0530

    changes in ip address

--- Desktop/RubyAssignment-1 /IPAddress.rb
+++ Desktop/RubyAssignment-1 /IPAddress.rb
@@ -1,21 +1,26 @@
-puts "Enter ip address:"
-ip = gets
-    if (ip.match(/^[0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}$/))
-        puts "ip address is valid"
-        d = ip.split
-        address = d[0].to_i
-    
-        if address>=1 && address<=100
-            puts "Class A"
-        elsif address>=101 && address<=200
-            puts "Class B"
-        elsif address>=201 && adddress<=300
-            puts "Class C"
-        else 
-            puts "Class D"
-        end
-    else 
-        puts "ip address is not valid"
-    end
+puts "enter ip adddress: "
+ip = gets 
 
+if  (ip.match(/^[0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}$/))
+    puts "ip is valid"
+    d = ip.split
+   address = d[0].to_i
+
+   if address>=1 && address<=127
+    puts "ClassA"
+   elsif address>=128 && address<=191
+    puts "Class B"
+   elsif address>=192 && address<=223
+    puts "class C"
+   elsif address>=224 && address<=239
+    puts "class D"
+   else 
+    puts "Class E"
+   end 
+else 
+    puts "invalid address"
+end 
+
+
+ 
         
